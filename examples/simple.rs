da218bae12527800ec939c7cf3b1ae98617571b2
Artem V. Ageev <artem.v.ageev@gmail.com>
2026-06-19 17:32:35 +0300

    init prj

---
+++ examples/simple.rs
@@ -0,0 +1,204 @@
+mod models {
+    #[derive(Default)]
+    pub struct Model(pub f64, pub f64);
+
+    impl Model {
+        pub fn set_cel(&mut self, value: f64) {
+            self.1 = (value * 9.0 / 5.0) + 32.0;
+        }
+        pub fn set_far(&mut self, value: f64) {
+            self.0 = (value - 32.0) * 5.0 / 9.0;
+        }
+    }
+}
+
+use easytk::prelude::*;
+
+#[cfg(target_os = "linux")]
+mod views {
+    use super::*;
+
+    pub enum Msg {
+        Cel(f64),
+        Far(f64),
+    }
+
+    #[derive(Default)]
+    pub struct View(gtk::Entry, gtk::Entry);
+
+    impl Component for View {
+        type Event = Msg;
+        type State = models::Model;
+        fn handle(msg: Self::Event, model: &mut Self::State, _: Sender<Self::Event>) -> bool {
+            match msg {
+                Msg::Cel(value) => model.set_cel(value),
+                Msg::Far(value) => model.set_far(value),
+            };
+            true
+        }
+        fn update(&self, model: &Self::State) {
+            self.0.update(&model.0.to_string());
+            self.1.update(&model.1.to_string());
+        }
+        fn view(&self, sender: Sender<Self::Event>) -> gtk::Box {
+            let pad = 10;
+            let wgt = gtk::Box::new(gtk::Orientation::Vertical, pad);
+            wgt.set_margin(pad);
+            wgt.add({
+                self.0.set_placeholder_text(Some("°C"));
+                self.0.set_hexpand(true);
+                self.0.connect_changed({
+                    let sender = sender.clone();
+                    move |entry| {
+                        if entry.has_visible_focus() {
+                            let value = entry.buffer().text().parse::<f64>().unwrap_or_default();
+                            sender.send(Msg::Cel(value)).unwrap();
+                        }
+                    }
+                });
+                &self.0
+            });
+            wgt.add({
+                self.1.set_placeholder_text(Some("°F"));
+                self.1.set_hexpand(true);
+                self.1.connect_changed({
+                    let sender = sender.clone();
+                    move |entry| {
+                        if entry.has_visible_focus() {
+                            let value = entry.buffer().text().parse::<f64>().unwrap_or_default();
+                            sender.send(Msg::Far(value)).unwrap();
+                        }
+                    }
+                });
+                &self.1
+            });
+            wgt
+        }
+    }
+}
+
+#[cfg(target_os = "linux")]
+fn main() -> gtk::glib::ExitCode {
+    views::View::run()
+}
+
+#[cfg(target_os = "windows")]
+mod views {
+    use super::*;
+
+    pub enum Msg {}
+
+    #[derive(Clone, Default)]
+    struct View();
+
+    impl Component for View {
+        type Event = Msg;
+        type State = bool;
+        fn handle(_msg: Self::Event, _model: &mut Self::State, _: Sender<Self::Event>) -> bool {
+            false
+        }
+        fn update(&mut self, _model: &Self::State) {}
+        fn view(&mut self, _sender: Sender<Self::Event>) -> impl WidgetExt {
+            let mut wgt = Wizard::default_fill();
+            wgt.set_frame(FrameType::FlatBox);
+            wgt.add(&info().with_label("Info"));
+            wgt.end();
+            wgt
+        }
+    }
+
+    fn info() -> Flex {
+        let mut wgt = Flex::default_fill();
+        wgt.set_frame(FrameType::FlatBox);
+        wgt.set_margin(PAD);
+        wgt.add(&{
+            let mut wgt = Flex::default().column();
+            wgt.set_frame(FrameType::RFlatBox);
+            wgt.set_color(Color::Background2);
+            wgt.set_pad(0);
+            wgt.set_margin(5);
+            wgt.add(&{
+                let mut wgt = HelpView::default();
+                wgt.set_color(Color::Background2);
+                wgt.set_scrollbar_size(LINE);
+                wgt.set_text_size(16);
+                wgt.set_frame(FrameType::FlatBox);
+                wgt.set_tooltip("README");
+                wgt.set_value(include_str!("../README.html"));
+                wgt
+            });
+            wgt.add(&{
+                let mut wgt = Frame::default();
+                wgt.config();
+                wgt
+            });
+            wgt.add(&{
+                let mut wgt = TextDisplay::default();
+                wgt.config();
+                wgt.set_frame(FrameType::FlatBox);
+                wgt.set_tooltip("LICENSE");
+                wgt.insert(include_str!("../LICENSE"));
+                wgt
+            });
+            wgt.end();
+            wgt.handle(Flex::listen);
+            wgt.handle_event(Event::Resize);
+            wgt
+        });
+        wgt.end();
+        wgt.handle(add_wizard);
+        wgt
+    }
+
+    fn add_wizard(wgt: &mut Flex, event: Event) -> bool {
+        let wizard = Wizard::from_dyn_widget(&wgt.parent().unwrap()).unwrap();
+        match event {
+            Event::Push => match event_mouse_button() {
+                MouseButton::Right => {
+                    let mut wgt = MenuButton::default();
+                    wgt.add_choice(
+                        &(0..wizard.children())
+                            .map(|x| {
+                                let label = wizard.child(x).unwrap().label();
+                                match wizard.try_current_widget().unwrap().label() == label {
+                                    true => format!("@->  {label}"),
+                                    false => format!("@-  {label}"),
+                                }
+                            })
+                            .collect::<Vec<String>>()
+                            .join("|"),
+                    );
+                    wgt.set_callback({
+                        let mut wizard = wizard.clone();
+                        move |menu| {
+                            if let Some(mut child) = wizard.child(menu.value()) {
+                                wizard.set_current_widget(&child);
+                                if let Some(mut window) = wizard.window() {
+                                    window.set_label(&format!(
+                                        "{}::{}",
+                                        window.xclass().unwrap(),
+                                        child.label()
+                                    ));
+                                };
+                                child.handle_event(Event::Resize);
+                            }
+                        }
+                    });
+                    wgt.popup();
+                    true
+                }
+                _ => false,
+            },
+            _ => false,
+        }
+    }
+}
+
+#[cfg(target_os = "windows")]
+fn main() -> Result<(), FltkError> {
+    View::run(Settings {
+        xclass: Some("Simple"),
+        icon: Some(SvgImage::from_data(include_str!("../../assets/logo.svg")).unwrap()),
+        ..Default::default()
+    })
+}
---
+++ src/lib.rs
@@ -0,0 +1,523 @@
+#[cfg(target_os = "windows")]
+pub mod prelude {
+    pub use {
+        fltk::{
+            app::{MouseButton, awake, event_coords, event_key, event_mouse_button},
+            browser::{Browser, BrowserType},
+            button::{Button, ButtonType},
+            dialog::{FileChooser, FileChooserType, alert_default, choice2_default},
+            draw,
+            enums::{Align, CallbackTrigger, Color, Cursor, Event, Font, FrameType, Key, Shortcut},
+            frame::Frame,
+            group::{Flex, FlexType, Scroll, Wizard},
+            image::{SharedImage, SvgImage},
+            input::{Input, InputType},
+            menu::{Choice, MenuButton, MenuButtonType, MenuFlag},
+            misc::{HelpView, Progress, Tooltip},
+            prelude::*,
+            text::{
+                StyleTableEntry, StyleTableEntryExt, TextAttr, TextBuffer, TextDisplay, TextEditor,
+                WrapMode,
+            },
+            valuator::{Counter, CounterType, Slider, SliderType},
+            window::Window,
+        },
+        std::{
+            sync::{
+                Arc, RwLock,
+                mpsc::{Receiver, Sender, channel},
+            },
+            thread,
+            time::{Duration, Instant},
+        },
+    };
+
+    #[derive(Default)]
+    pub struct Settings {
+        pub fullscreen: bool,
+        pub size: Option<(i32, i32)>,
+        pub font_size: Option<u8>,
+        pub font: Option<Font>,
+        pub xclass: Option<&'static str>,
+        pub icon: Option<SvgImage>,
+    }
+
+    impl Settings {
+        pub fn config(&self) -> Window {
+            app::set_scheme(app::Scheme::Base);
+            app::set_frame_type2(FrameType::UpBox, FrameType::ThinUpBox);
+            app::set_frame_type2(FrameType::DownBox, FrameType::ThinDownBox);
+            app::set_background_color(238, 232, 213);
+            app::set_background2_color(253, 246, 227);
+            app::set_foreground_color(88, 110, 117);
+            app::set_selection_color(203, 75, 22);
+            app::set_inactive_color(181, 137, 0);
+            Tooltip::set_color(Color::Background2);
+            Tooltip::set_text_color(Color::Foreground);
+            for (color, (r, g, b)) in [
+                (Color::Red, (220, 50, 47)),
+                (Color::Magenta, (211, 54, 130)),
+                (Color::Blue, (38, 139, 210)),
+                (Color::Cyan, (42, 161, 152)),
+                (Color::Green, (133, 153, 0)),
+                (Color::Yellow, (181, 137, 0)),
+            ] {
+                app::set_color(color, r, g, b);
+            }
+            app::set_visible_focus(false);
+            app::set_font(match cfg!(target_os = "windows") {
+                true => Font::by_name("BCascadia Mono"),
+                false => self.font.unwrap_or(Font::CourierBold),
+            });
+            app::set_font_size(self.font_size.unwrap_or(14));
+            let (w, h) = self.size.unwrap_or((400, 640));
+            let mut wgt = Window::default().with_size(w, h).center_screen();
+            wgt.set_xclass(self.xclass.unwrap_or("FLTK"));
+            wgt.set_label(self.xclass.unwrap_or("FLTK"));
+            wgt.size_range(w, h, 0, 0);
+            wgt.fullscreen(self.fullscreen);
+            wgt.make_resizable(true);
+            wgt.set_icon(self.icon.clone());
+            wgt.end();
+            wgt.show();
+            wgt
+        }
+    }
+
+    pub trait Component
+    where
+        Self: Default + Clone + 'static,
+    {
+        type State: Default + 'static;
+        type Event: 'static;
+        fn handle(msg: Self::Event, model: &mut Self::State, sender: Sender<Self::Event>) -> bool;
+        fn update(&mut self, model: &Self::State);
+        fn view(&mut self, sender: Sender<Self::Event>) -> impl WidgetExt;
+        fn mount(&mut self) -> impl WidgetExt {
+            let (sender, resiver) = channel::<Self::Event>();
+            let mut model = Self::State::default();
+            self.update(&model);
+            const TICK: f64 = 0.02;
+            app::add_timeout3(TICK, {
+                let mut page = self.clone();
+                let sender = sender.clone();
+                move |handle| {
+                    if let Ok(msg) = resiver.try_recv()
+                        && Self::handle(msg, &mut model, sender.clone())
+                    {
+                        page.update(&model);
+                    }
+                    app::repeat_timeout3(TICK, handle);
+                }
+            });
+            self.view(sender)
+        }
+        fn run(settings: Settings) -> Result<(), FltkError> {
+            let app = app::App::default().load_system_fonts();
+            let mut wgt = settings.config();
+            wgt.begin();
+            wgt.add(&Self::default().mount());
+            wgt.end();
+            wgt.set_callback(move |wgt| {
+                if app::event() == Event::Close {
+                    wgt.hide();
+                }
+            });
+            app.run()
+        }
+    }
+    pub trait Update<T>
+    where
+        Self: WidgetExt,
+    {
+        fn update(&mut self, value: T);
+    }
+
+    impl Update<&String> for TextBuffer {
+        fn update(&mut self, value: &String) {
+            if self.text() != *value {
+                self.set_text(value);
+            };
+        }
+    }
+
+    impl Update<&String> for TextDisplay {
+        fn update(&mut self, value: &String) {
+            let mut buffer = match self.buffer() {
+                Some(buf) => buf,
+                None => {
+                    self.set_buffer(TextBuffer::default());
+                    self.buffer().unwrap()
+                }
+            };
+            if buffer.text() != *value {
+                buffer.set_text(value);
+                self.scroll(buffer.text().lines().count() as i32, 0);
+            };
+        }
+    }
+
+    impl Update<&String> for TextEditor {
+        fn update(&mut self, value: &String) {
+            let mut buffer = self.buffer().unwrap();
+            if buffer.text() != *value {
+                buffer.set_text(value);
+            };
+        }
+    }
+
+    impl Update<&String> for Input {
+        fn update(&mut self, value: &String) {
+            if self.value() != *value {
+                self.set_value(value);
+            };
+        }
+    }
+
+    impl Update<&String> for Frame {
+        fn update(&mut self, value: &String) {
+            if self.label() != *value {
+                self.set_label(value);
+            };
+        }
+    }
+
+    impl Update<(Vec<String>, i32)> for Choice {
+        fn update(&mut self, value: (Vec<String>, i32)) {
+            if self.size() != value.0.len() as i32 {
+                self.clear();
+                if !value.0.is_empty() {
+                    self.add_choice(&value.0.join("|").replace(r#"/"#, r#"\/"#));
+                }
+            };
+            match self.size() > 0 {
+                true => self.activate(),
+                false => self.deactivate(),
+            }
+            if self.value() != value.1 {
+                self.set_value(value.1);
+            };
+        }
+    }
+
+    impl Update<i32> for Choice {
+        fn update(&mut self, value: i32) {
+            if self.value() != value {
+                self.set_value(value);
+            };
+        }
+    }
+
+    impl Update<(f64, String)> for Progress {
+        fn update(&mut self, value: (f64, String)) {
+            if self.value() != value.0 {
+                self.set_value(value.0);
+            };
+            if self.label() != value.1 {
+                self.set_label(&value.1)
+            };
+        }
+    }
+
+    impl Update<f64> for Slider {
+        fn update(&mut self, value: f64) {
+            if self.value() != value {
+                self.set_value(value);
+            };
+        }
+    }
+
+    impl Update<bool> for Button {
+        fn update(&mut self, value: bool) {
+            if self.active() != value {
+                match value {
+                    true => self.activate(),
+                    false => self.deactivate(),
+                };
+            };
+        }
+    }
+
+    impl Update<bool> for Slider {
+        fn update(&mut self, value: bool) {
+            if self.active() != value {
+                match value {
+                    true => self.activate(),
+                    false => self.deactivate(),
+                };
+            };
+        }
+    }
+
+    impl Update<(Option<String>, i32)> for Frame {
+        fn update(&mut self, value: (Option<String>, i32)) {
+            if self.tooltip() != value.0 {
+                if let Some(tooltip) = value.0 {
+                    self.set_tooltip(&tooltip);
+                };
+                self.do_callback();
+            };
+            if self.label_size() != value.1 {
+                self.set_label_size(value.1);
+                self.do_callback();
+            };
+        }
+    }
+
+    pub trait Config
+    where
+        Self: 'static,
+    {
+        fn config(&mut self);
+    }
+
+    impl Config for TextDisplay {
+        fn config(&mut self) {
+            self.set_align(Align::Left);
+            self.set_trigger(CallbackTrigger::Changed);
+            self.set_buffer(TextBuffer::default());
+            self.set_linenumber_width(0);
+            self.set_text_size(16);
+            self.set_scrollbar_size(LINE);
+            self.wrap_mode(WrapMode::AtBounds, 0);
+        }
+    }
+
+    impl Config for Button {
+        fn config(&mut self) {
+            self.set_label_size(HEIGHT_FONT);
+        }
+    }
+
+    impl Config for Slider {
+        fn config(&mut self) {
+            self.set_align(Align::Left);
+            self.set_type(SliderType::HorizontalNice);
+            self.set_color(Color::Background2);
+        }
+    }
+
+    impl Config for Frame {
+        fn config(&mut self) {
+            self.set_frame(FrameType::FlatBox);
+            self.handle(Self::listen);
+        }
+    }
+
+    impl Config for Counter {
+        fn config(&mut self) {
+            self.set_align(Align::Left);
+            self.set_type(CounterType::Simple);
+            self.set_precision(0);
+        }
+    }
+
+    impl Config for Progress {
+        fn config(&mut self) {
+            self.set_selection_color(Color::Selection);
+        }
+    }
+
+    impl Config for TextEditor {
+        fn config(&mut self) {
+            self.set_align(Align::Left);
+            self.set_trigger(CallbackTrigger::Changed);
+            self.set_linenumber_width(0);
+            self.set_buffer(TextBuffer::default());
+            self.set_text_size(14);
+            self.set_scrollbar_size(LINE);
+            self.wrap_mode(WrapMode::AtBounds, 0);
+            self.kf_end();
+        }
+    }
+
+    pub trait Listener
+    where
+        Self: 'static,
+    {
+        fn listen(wgt: &mut Self, event: Event) -> bool;
+    }
+
+    impl Listener for Frame {
+        fn listen(wgt: &mut Self, event: Event) -> bool {
+            let mut flex = Flex::from_dyn_widget(&wgt.parent().unwrap()).unwrap();
+            match event {
+                Event::Push => true,
+                Event::Drag => {
+                    let child0 = flex.child(0).unwrap();
+                    let child2 = flex.child(2).unwrap();
+                    match flex.get_type() {
+                        FlexType::Column => {
+                            let y = flex.margin() * 2 + wgt.h();
+                            if (y..(flex.h() - y) / 2).contains(&app::event_y()) {
+                                flex.fixed(&child2, flex.h() - app::event_y());
+                                flex.fixed(&child0, 0);
+                            } else if ((flex.h() - y) / 2..=(flex.h() - y))
+                                .contains(&app::event_y())
+                            {
+                                flex.fixed(&child2, 0);
+                                flex.fixed(&child0, app::event_y());
+                            }
+                        }
+                        FlexType::Row => {
+                            let x = flex.margin() * 2 + wgt.w();
+                            if (x..=(flex.w() - x) / 2).contains(&app::event_x()) {
+                                flex.fixed(&child2, flex.w() - app::event_x());
+                                flex.fixed(&child0, 0);
+                            } else if ((flex.w() - x) / 2..=(flex.w() - x))
+                                .contains(&app::event_x())
+                            {
+                                flex.fixed(&child0, app::event_x());
+                                flex.fixed(&child2, 0);
+                            }
+                        }
+                    }
+                    app::redraw();
+                    true
+                }
+                Event::Enter => {
+                    draw::set_cursor(match flex.get_type() {
+                        FlexType::Column => Cursor::NS,
+                        FlexType::Row => Cursor::WE,
+                    });
+                    true
+                }
+                Event::Leave => {
+                    draw::set_cursor(Cursor::Arrow);
+                    true
+                }
+                _ => false,
+            }
+        }
+    }
+
+    impl Listener for Flex {
+        fn listen(wgt: &mut Self, event: Event) -> bool {
+            if event == Event::Resize {
+                if wgt.children() == 3 {
+                    wgt.set_type(match wgt.w() < wgt.h() {
+                        true => FlexType::Column,
+                        false => FlexType::Row,
+                    });
+                    wgt.fixed(&wgt.child(0).unwrap(), 0);
+                    wgt.fixed(&wgt.child(1).unwrap(), LINE);
+                    wgt.fixed(&wgt.child(2).unwrap(), 0);
+                }
+                return true;
+            }
+            false
+        }
+    }
+
+    pub fn choice_file(filter: &str) -> Option<String> {
+        let mut dialog = FileChooser::new(
+            std::env::var(match cfg!(target_os = "windows") {
+                true => "HOMEPATH",
+                false => "HOME",
+            }).unwrap(),
+            filter,
+            match filter.is_empty() {
+                true => FileChooserType::Directory,
+                false => FileChooserType::Create,
+            },
+            "Choce ...",
+        );
+        dialog.show();
+        while dialog.shown() {
+            app::wait();
+        }
+        match dialog.count() {
+            0 => None,
+            _ => dialog.value(1),
+        }
+    }
+
+    pub fn choice_files(filter: &str) -> Option<Vec<String>> {
+        let mut dialog = FileChooser::new(
+            std::env::var(match cfg!(target_os = "windows") {
+                true => "HOMEPATH",
+                false => "HOME",
+            }).unwrap(),
+            filter,
+            FileChooserType::Multi,
+            "Choose File...",
+        );
+        dialog.show();
+        while dialog.shown() {
+            app::wait();
+        }
+        match dialog.count() {
+            0 => None,
+            _ => Some(
+                (1..=dialog.count())
+                    .filter_map(|x| dialog.value(x))
+                    .collect(),
+            ),
+        }
+    }
+}
+
+#[cfg(target_os = "linux")]
+pub mod prelude {
+    pub use {gtk::prelude::*, std::sync::mpsc::Sender};
+
+    pub trait Component
+    where
+        Self: Default + 'static,
+    {
+        type Event: 'static;
+        type State: Default + 'static;
+        fn handle(msg: Self::Event, model: &mut Self::State, sender: Sender<Self::Event>) -> bool;
+        fn update(&self, model: &Self::State);
+        fn view(&self, sender: Sender<Self::Event>) -> gtk::Box;
+        fn mount() -> gtk::Box {
+            let (sender, resiver) = std::sync::mpsc::channel::<Self::Event>();
+            let mut model = Self::State::default();
+            let page = Self::default();
+            page.update(&model);
+            let view = page.view(sender.clone());
+            gtk::glib::timeout_add_local(std::time::Duration::from_millis(20), {
+                let sender = sender.clone();
+                move || {
+                    if let Ok(msg) = resiver.try_recv()
+                        && Self::handle(msg, &mut model, sender.clone())
+                    {
+                        page.update(&model);
+                    }
+                    gtk::glib::ControlFlow::Continue
+                }
+            });
+            view
+        }
+        fn run() -> gtk::glib::ExitCode {
+            let app = gtk::Application::builder()
+                .application_id("io.gitlab.kbit")
+                .build();
+            app.connect_activate(move |app| {
+                gtk::ApplicationWindow::builder()
+                    .application(app)
+                    .width_request(640)
+                    .height_request(400)
+                    .child(&Self::mount())
+                    .build()
+                    .show_all();
+            });
+            app.run()
+        }
+    }
+
+    pub trait Update<T>
+    where
+        Self: WidgetExt,
+    {
+        fn update(&self, value: T);
+    }
+
+    impl Update<&String> for gtk::Entry {
+        fn update(&self, value: &String) {
+            if !self.has_visible_focus() {
+                self.buffer().set_text(value);
+            }
+        }
+    }
+}
